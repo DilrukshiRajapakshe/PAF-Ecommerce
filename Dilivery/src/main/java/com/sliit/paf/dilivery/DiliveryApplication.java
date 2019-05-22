83f162a47b877a66171e9a2303cebfd3e1af782c
Dirukshi Rajapakshe <dilrukshi.rajapakshe@gmail.com>
2019-05-22 13:09:01 +0530

    dilivry

---
+++ Dilivery/src/main/java/com/sliit/paf/dilivery/DiliveryApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.paf.dilivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiliveryApplication.class, args);
+    }
+
+}
